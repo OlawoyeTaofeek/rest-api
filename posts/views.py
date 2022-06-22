15863010ef4b01577ed01caf459a7efa1ece4299
Olawoye Taofeek <habephe21@gmail.com>
2022-06-22 19:39:59 +0100

    django app and project created

--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,9 @@
 
 from django.shortcuts import render
 from rest_framework import generics, permissions
-from .models import Post
+from .models import Post, Vote
 from .serializers import PostSerializer, VoteSerializer
+from rest_framework.exceptions import ValidationError
 # Create your views here.
 
 class PostList(generics.ListCreateAPIView):
@@ -21,7 +22,9 @@ class VoteCreate(generics.CreateAPIView):
     def get_queryset(self):
         user = self.request.user
         post = Post.objects.get(pk=self.kwargs['pk'])
-        return Vote.object.filter(voter=user, post=post)
+        return Vote.objects.filter(voter=user, post=post)
 
     def perform_create(self, serializer):
+        if self.get_queryset().exists():
+            raise ValidationError('You have already voted for this post :)')
         serializer.save(voter=self.request.user, post=Post.objects.get(pk=self.kwargs['pk']))
