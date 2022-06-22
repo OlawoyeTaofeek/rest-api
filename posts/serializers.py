0957533d527bdafa80943a339a258a2c48cf758d
Olawoye Taofeek <habephe21@gmail.com>
2022-06-22 19:13:52 +0100

    django app and project created

--- posts/serializers.py
+++ posts/serializers.py
@@ -4,6 +4,15 @@ from .models import Post, Vote
 
 
 class PostSerializer(serializers.ModelSerializer):
+
+    poster_name = serializers.ReadOnlyField(source= 'poster_name.username')
+    poster_id = serializers.ReadOnlyField(source= 'poster_name.id')
+
     class Meta:
         model = Post
-        fields = ['id', 'title', 'url', 'poster_name', 'created']
+        fields = ['id', 'title', 'url', 'poster_name', 'poster_id', 'created']
+
+class VoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Vote
+        fields = ['id']
--- posts/views.py
+++ posts/views.py
@@ -1,11 +1,27 @@
 
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .models import Post
-from .serializers import PostSerializer
+from .serializers import PostSerializer, VoteSerializer
 # Create your views here.
 
 class PostList(generics.ListCreateAPIView):
     serializer_class = PostSerializer
     queryset = Post.objects.all()
     serializer = PostSerializer(queryset, many=True)
+    permission_class = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(poster_name=self.request.user)
+
+class VoteCreate(generics.CreateAPIView):
+    serializer_class = VoteSerializer
+    permission_class = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        post = Post.objects.get(pk=self.kwargs['pk'])
+        return Vote.object.filter(voter=user, post=post)
+
+    def perform_create(self, serializer):
+        serializer.save(voter=self.request.user, post=Post.objects.get(pk=self.kwargs['pk']))
